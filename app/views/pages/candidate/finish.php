93aa6b714417b9e091fda6319b73593aed9c20d2
nicartfranz <nicartfranz@gmail.com>
2020-08-24 00:02:50 +0800

    Update finish.php

--- app/views/pages/candidate/finish.php
+++ app/views/pages/candidate/finish.php
@@ -2,9 +2,9 @@
 
     <div class="jumbotron">
         <h4 class="display-8">You have completed the test.</h4>
-        <p class="lead">This is a welcome message, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+<!--        <p class="lead">This is a welcome message, a simple jumbotron-style component for calling extra attention to featured content or information.</p>-->
         <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+<!--        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>-->
         <a href="<?= APP_BASE_URL ?>candidate/" class="btn btn-success">&nbsp;&nbsp;&nbsp;OK&nbsp;&nbsp;&nbsp;</a>
     </div>
     
